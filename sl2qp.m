9b32914168ae174d73ebbeae9b689b54bb79418a
Ari Markowitz <ariymarkowitz@gmail.com>
2024-11-22 10:45:39 +1300

    Attempt discreteness code

--- sl2qp.m
+++ sl2qp.m
@@ -1,4 +1,4 @@
-// The minimum valuation of the entries of a matrix
+/* The minimum valuation of the entries of a matrix. */
 function minValuation(M, P)
     return Minimum([Valuation(y, P) : y in Eltseq(M)]);
 end function;
@@ -11,7 +11,9 @@ declare attributes BTTVert:
     matrix, // Matrix
     precision; // Precision
 
-/* Tree intrinsics */
+/**
+ * Tree intrinsics
+ */
 
 intrinsic BruhatTitsTree(place::PlcNumElt) -> BTTree
 {Returns a BTTree localised at a given place.}
@@ -167,18 +169,18 @@ intrinsic CmpVertices(v, w) -> RngIntElt
         return 1;
     end if;
 
-    // Projection of the paths along the main axis
+    // Projection of the paths along the main axis.
     vproj := Minimum(Valuation(Value(v), P), Precision(v));
     wproj := Minimum(Valuation(Value(w), P), Precision(w));
     
-    // If has smaller displacement along the main axis, it is smaller
+    // If has smaller displacement along the main axis, it is smaller.
     if vproj lt wproj then
         return -1;
     elif vproj gt wproj then
         return 1;
     end if;
 
-    // Compare the first edge at which they differ
+    // Compare the first edge at which they differ.
     d := Value(v) - Value(w);
     val := Valuation(d, P);
 
@@ -194,12 +196,12 @@ intrinsic CmpVertices(v, w) -> RngIntElt
     error "Unreachable state: vertices were equal but comparison failed";
 end intrinsic;
 
-// The vertex halfway between the origin and v
+/* The vertex halfway between the origin and v */
 function halfPath(v)
     T := Parent(v);
     P := Place(T);
 
-    // Distance to origin = fall + rise
+    // Distance to origin = fall + rise.
     fall := -Minimum(Minimum(Valuation(Value(v), P), Precision(v)), 0);
     rise := Precision(v) + fall;
 
@@ -241,3 +243,267 @@ intrinsic Print(v::BTTVert)
 {Prints a vertex.}
     printf "Vertex with value %o and precision %o", Value(v), Precision(v);
 end intrinsic;
+
+/**
+ * Other intrinsics
+ */
+
+intrinsic IsElliptic(M::AlgMatElt[FldNum], T::BTTree) -> BoolElt
+{Returns whether a matrix is elliptic.}
+    return 2*Valuation(Trace(M), Place(T)) gt Valuation(Determinant(M), Place(T));
+end intrinsic;
+
+/**
+ * Discreteness algorithms
+ */
+
+/* Evaluate a word (as a list of ids) with a generating set. */
+function evaluate_word(word_ids, seq)
+    return &*[seq[i] : i in word_ids];
+end function;
+
+/* Get the symmetric sequence of generators and inverses. */
+symmetrize := function(gens)
+  gens_sym := &cat[[x, x^-1] : x in gens];
+  if Nresults() eq 1 then return gens_sym; end if;
+  S := Sym(#gens_sym);
+  invert_perm := S!(&cat[[2*i, 2*i-1] : i in [1 .. #gens]]);
+  return gens_sym, invert_perm;
+end function;
+
+/* A generating set for a subgroup of SL2 over a non-Archimedean local field. */
+declare type GrpSL2pGen;
+declare attributes GrpSL2KGen:
+    field, // Base field
+    matalg, // Matrix Algebra
+    tree, // Bruhat-T its tree
+    seq, // The original generators
+    psl, // Whether the generators should be considered a subgroup of PSL
+    has_neg, // True if psl is false and the group contains -I
+    neg_word, // -I as a word in the generators
+    /**
+    * "un" - Unknown type (only used for intermediate reduction steps)
+    * "df" - Discrete and free
+    * "el" - Contains an elliptic element
+    */
+    type,
+    /**
+    * Proof of the type of the group:
+    * "un" - Nielsen-equivalent generating set (for intermediate steps)
+    * "df" - Reduced generating set (does not include -I)
+    * "el" - Elliptic element
+    */
+    witness,
+    witness_word, // A word corresponding to each element of the witness
+
+    // These attributes are only defined if the group is determined to be discrete and free.
+    FPgrp, // Finite presentation
+    matgrp, // Matrix group (with reduced generating set)
+    iso; // Isometry to finitely presented group
+
+intrinsic Print(gen::GrpSL2KGen)
+{ Print the generating set. }
+list := &*[Sprint(x) * (i lt #gen`seq select "\n\n" else "") : i -> x in gen`seq];
+printf "Generators of subgroup of %oSL2 over non-Archimedean field:\n%o", gen`psl select "P" else "", gen`field, list;
+end intrinsic;
+intrinsic SL2KGens(seq::SeqEnum[AlgMatElt[FldNum]], place::PlcNumElt : psl := false) -> GrpSL2Gen
+{ Create a generating set for a subgroup of SL2 over an algebraic number field at a finite place. }
+  gen := New(GrpSL2KGen);
+  require IsFinite(place): "Place must be finite";
+  require Degree(Universe(seq)) eq 2: "Matrix algebra must be degree 2";
+  require &and[Determinant(g) eq 1 : g in seq]: "Matrices must have determinant 1";
+  require BaseRing(Universe(seq)) eq NumberField(place): "Generators and place must have the same base field";
+  gen`seq := seq;
+  gen`matalg := Universe(seq);
+  gen`field := BaseRing(gen`matalg);
+  gen`tree := BruhatTitsTree(place);
+  gen`psl := psl;
+  gen`has_neg := false;
+
+  gen`type := "un";
+  gen`witness := seq;
+  gen`witness_word := Setseq(Generators(FreeGroup(#seq)));
+  return gen;
+end intrinsic;
+
+/* Apply one step of the reduction algorithm. */
+procedure reduce_step(gen)
+    if gen`type ne "un" then return; end if;
+
+    // Deal with elliptic, I, and -I elements.
+    for i -> g in gen`seq do
+        if IsOne(g) or IsOne(-g) then
+            if IsOne(-g) and not gen`psl and not gen`has_neg then
+                gen`has_neg := true;
+                gen`neg_word := gen`witness_word[i];
+            end if;
+            Remove(~gen`witness, i);
+            Remove(~gen`witness_word, i);
+            return;
+        elif is_elliptic(g, tree) then
+            gen`type := "el";
+            gen`witness := g;
+            gen`witness_word := gen`witness_word[i];
+            return;
+        end if;
+    end for;
+
+    // A single non-elliptic element generates a free group.
+    if #gen`witness le 1 then
+        gen`type := "df";
+        gen`FPgrp := FreeGroup(#gen`witness_word);
+        if gen`has_neg then
+            gen`FPgrp := DirectProduct(gen`FPgrp, CyclicGroup(GrpFP, 2));
+        end if;
+        return;
+    end if;
+
+    gens_sym, inv := symmetrize(gen`witness);
+    words_sym := symmetrize(gen`witness_word);
+    for i -> x in gens_sym do
+        for j -> y in gens_sym do
+            if Ceiling(i/2) eq Ceiling(j/2) then continue; end if;
+            z := x*y;
+            if CmpIsometry(x, z) eq 1 then
+                x := z;
+                id := Ceiling(i/2);
+                gen`witness[id] := z;
+                gen`witness_word[id] := words_sym[i]*words_sym[j];
+            end if;
+        end for;
+    end for;
+
+    // The group is discrete and free.
+    gen`type := "df";
+    gen`FPgrp := FreeGroup(#gen`witness_word);
+    if gen`has_neg then
+        gen`FPgrp := DirectProduct(gen`FPgrp, CyclicGroup(GrpFP, 2));
+    end if;
+end procedure;
+
+intrinsic RecognizeDiscreteFree(gen::GrpSL2KGen) -> GrpSL2KGen
+{ Decide if a finite subset of SL2 over a non-Archimedean local field is discrete and free.
+Modifies the attributes of the input generating set. }
+    repeat
+        reduce_step(gen);
+    until gen`type ne "un";
+
+    // Construct isomorphism between presentation and matrix group.
+    if IsDiscreteFree(gen) then
+        gen`matgrp := MatrixGroup<2, gen`field | ReducedGenerators(gen)>;
+        
+        to_mat := hom<gen`FPgrp -> gen`matgrp | [gen`matgrp!g : g in ReducedGenerators(gen)]>;
+        to_fp_fn := function(g)
+            b, w := IsElementOf(Matrix(g), gen);
+            assert b;
+            return w;
+        end function;
+
+        gen`iso := iso<gen`matgrp -> gen`FPgrp | g :-> to_fp_fn(g), w :-> to_mat(w)>;
+    end if;
+end intrinsic;
+
+intrinsic IsDiscreteFree(gen::GrpSL2KGen) -> BoolElt
+{ Returns whether a finite subset of SL2 over a non-Archimedean local field is discrete and free. }
+    error if gen`type eq "un", "The group type is unknown; prepare it using `RecognizeDiscreteFree`";
+    return gen`type eq "df";
+end intrinsic;
+
+intrinsic ReducedGenerators(gen::GrpSL2KGen) -> SeqEnum[AlgMatElt]
+{ Return a reduced generating set for a discrete torsion-free group. }
+    error if gen`type eq "un", "The group must be prepared using `RecognizeDiscreteFree`";
+    error if not IsDiscreteFree(gen), "The group is not discrete and free";
+    if (gen`has_neg) then
+        return gen`witness cat [-One(BaseField(gen))];
+    else
+        return gen`witness;
+    end if;
+end intrinsic;
+
+intrinsic BaseField(gen::GrpSL2KGen) -> FldNum
+{ Return the base field of the group. }
+     return gen`field;
+end intrinsic;
+
+intrinsic HasNegativeOne(gen::GrpSL2KGen) -> FldNum, GrpFPElt
+{ Return true if the subgroup of SL(2, K) has -I. }
+    error if gen`type eq "un", "The group must be prepared using `RecognizeDiscreteFree`";
+    error if gen`psl, "The group must be a subgroup of SL(2, K)";
+    if gen`has_neg then
+        return true, gen`neg_word;
+    else
+        return false;
+    end if;
+end intrinsic;
+
+intrinsic Rank(gen::GrpSL2KGen) -> RngIntElt
+{ The rank of a discrete free group. }
+    error if gen`type eq "un", "The group must be prepared using `RecognizeDiscreteFree`";
+    error if not IsDiscreteFree(gen), "The group is not discrete and free";
+    return gen`has_neg select #gen`witness + 1 else #gen`witness;
+end intrinsic
+
+intrinsic IsElementOf(g::AlgMatElt[FldNum], gen::GrpSL2KGen) -> BoolElt, GrpFPElt
+{ Check if a matrix is in the group. }
+    error if gen`type eq "un", "The group must be prepared using `RecognizeDiscreteFree`";
+    error if not IsDiscreteFree(gen), "The group is not discrete and free";
+
+    gen_sym := symmetrize(gen`witness);
+    G := gen`FPgrp;
+    fp_sym := symmetrize(Setseq(Generators(G)));
+    g_word := Id(G);
+    o := Origin(gen`tree);
+    repeat
+        finish := true;
+        for i -> x in gen_sym do
+            h := g*x;
+            if CmpVertices(o*h, o*g) eq -1 then
+                g_word := g_word*fp_sym[i];
+                g := g*x;
+                finish := false
+                break;
+            end if;
+        end for;
+    until finish;
+    if IsOne(g) or (gen`psl and IsOne(-g)) then
+        return true, g_word^-1;
+    elif gen`has_neg and IsOne(-g) then
+        return true, g_word^-1*G.(NumberOfGenerators(G));
+    else
+        return false, _;
+    end if;
+end intrinsic;
+
+intrinsic MapToFundamentalDomain(v::BTTVert, gen::GrpSL2KGen) -> BTTVert
+{ Return the unique g (and corresponding word) such that v*g is in the fundamental domain.}
+    error if gen`type eq "un", "The group must be prepared using `RecognizeDiscreteFree`";
+    error if not IsDiscreteFree(gen), "The group is not discrete and free";
+    error if not Parent(v) eq gen`tree, "The vertex must be in the same tree as the group";
+
+    gen_sym, inv := symmetrize(gen`witness);
+    G := gen`FPgrp;
+    fp_sym := symmetrize(Setseq(Generators(G)));
+    g := One(gen`matalg);
+    g_word := Id(G);
+    w := v;
+
+    repeat
+        finish := true;
+        for i -> x in gen_sym do
+            w2 := w*x;
+            if CmpVertices(w2, w) eq -1 then
+                g_word := g_word*fp_sym[i];
+                g := g*x;
+                w := w2;
+                finish := false;
+                break;
+            end if;
+        end for;
+    until finish;
+
+    return g, g_word;
+end intrinsic;
+
+/**
+ * Discreteness algorithms
+ */
